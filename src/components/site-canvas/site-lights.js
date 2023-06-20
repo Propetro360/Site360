660fcc44ceb4d38d4d2443f9ecd2d6cd6b25eab0
ProPetro-JigarPatel <jigar.patel@propetroservices.com>
2023-06-20 10:46:42 -0500

    update three fiber packages, remove environment

--- src/components/site-canvas/site-lights.js
+++ src/components/site-canvas/site-lights.js
@@ -3,9 +3,9 @@ import { Environment, GizmoHelper, GizmoViewcube } from "@react-three/drei";
 function SiteLights(props) {
   return (
     <>
-      {/* <pointLight position={[10, 10, 10]} intensity={0.5} />
-      <spotLight position={[0, -1, -5]} intensity={1} castShadow /> */}
-      <Environment preset="apartment" />
+      <pointLight position={[10, 10, 10]} intensity={0.5} />
+      <spotLight position={[0, -1, -5]} intensity={1} castShadow />
+      {/* <Environment preset="apartment" /> */}
       <pointLight position={[10, 10, 10]} intensity={0.5} />
       <GizmoHelper alignment="top-right" renderPriority={99}>
         <GizmoViewcube />
